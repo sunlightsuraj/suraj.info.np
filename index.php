d0d559e15a78cfe9815f56b9e603edd64977b6c3
Suraj Shrestha <suraj.sht@hotmail.com>
2017-07-23 00:40:37 +0545

    suraj.info.np

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: suraj
+ * Date: 7/22/17
+ * Time: 9:42 PM
+ */
+?>
+
+<html>
+<head>
+    <title>Hello From Suraj</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+    <div class="about-me">
+        <h1>Hello, It's me Suraj.</h1>
+        <h3>I'm a Software Engineer, FOSS Activist and Tech Lover.</h3>
+        <h3>If you wanna drop a mail me, you can <i><a href="mailto:pingme@suraj.info.np">pingme@suraj.info.np</a></i></h3>
+        <h3>Or you can find me here:</h3>
+        <ul class="social-links">
+            <li><a href="https://facebook.com/sunlightsuraj" target="_blank"><img src="assets/image/social/fb.png" alt="fb"></a> </li>
+            <li><a href="https://www.linkedin.com/in/sunlightsuraj/" target="_blank"><img
+                            src="assets/image/social/linkedin.png" alt="linkedin"></a> </li>
+            <li><a href="https://plus.google.com/u/0/+SurajShrestha1602" target="_blank"><img src="assets/image/social/gplus.png"
+                                                                              alt="google plus"></a></li>
+            <li><a href="https://twitter.com/sunlight_suraj" target="_blank"><img src="assets/image/social/twitter.png" alt="twitter"></a> </li>
+        </ul>
+    </div>
+    <div class="my-image">
+        <img src="assets/image/suraj.png" alt="Suraj Shrestha" class="img-responsive">
+    </div>
+</div>
+<script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
